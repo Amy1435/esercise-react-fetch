0850b8d928e220ae3636794fb6db0af9340e2c3a
Amanda <amanda.ausejo@gmail.com>
2023-12-12 15:16:00 +0100

    esercizio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import CountryCards from "./CountryCard";
 
 function App() {
+    const [countryData, setCountryData] = useState([
+        {
+            countryName: "Italy",
+            flagUrl:
+                "https://www.mrflag.com/wp-content/uploads/2018/10/Italy-Flag-100x100.png",
+            population: "59,11 milions",
+            capital: "Rome",
+        },
+        {
+            countryName: "Usa",
+            flagUrl:
+                "https://www.mrflag.com/wp-content/uploads/2018/10/USA-United-States-Flag-100x100.png",
+            population: "331,9 milions",
+            capital: "Washington",
+        },
+    ]);
     return (
-        <CountryCards
-            countryName={"italy"}
-            flagUrl={
-                "https://www.mrflag.com/wp-content/uploads/2018/10/Italy-Flag-100x100.png"
-            }
-            population={"59,11 milioni"}
-            capital={"Rome"}
-        />
+        <>
+            {countryData.map((country, i) => (
+                <CountryCards
+                    key={i}
+                    countryName={country.countryName}
+                    flagUrl={country.flagUrl}
+                    population={country.population}
+                    capital={country.capital}
+                />
+            ))}
+        </>
     );
 }
 
